abb10c461658649f13dcd03af3d33889acbad9e9
Saif Ali <saif.tharani95@gmail.com>
2020-06-16 01:28:55 +0500

    Setup userModel, created User, and implemented hashing function for passwords.

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -24,7 +25,20 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         required: [true, 'Please confirm the password.'],
+        validate: {
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: 'The passwords does not match!',
+        },
     },
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const app = express();
+
 const {
     allUsers,
     createUser,
     getUser,
     updateUser,
-    deleteUser
-} = require('./../controllers/userController');
+    deleteUser,
+} = require('../controllers/userController');
 
+const { signUp } = require('../controllers/authController');
 
 const userRouter = express.Router();
 //Using app.route for furthering restructuring the routes - Users' Routes
+
+//Routes defined for normal users.
+userRouter.post('/signup', signUp);
+
+//Routes defined for system administrator!
 userRouter.route('/').get(allUsers).post(createUser);
 userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
