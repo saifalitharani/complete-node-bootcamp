a315d17321cad44e2ac2098beb00903eb24eadb2
Saif Ali <saif.tharani95@gmail.com>
2020-06-22 18:48:30 +0500

    Implemented HTTP Parameter Pollution prevention middleware.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -28,6 +29,20 @@ app.use(mongoSanitize());
 
 //Data sanitization against XSS
 app.use(xss());
+
+//Prevent parameter pollution
+app.use(
+    hpp({
+        whitelist: [
+            'duration',
+            'ratingsQuantity',
+            'ratingsAverage',
+            'maxGroupSize',
+            'difficulty',
+            'price',
+        ],
+    })
+);
 
 //Limit request from same api over certain period of time.
 const limiter = rateLimit({
