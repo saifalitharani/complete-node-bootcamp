4b06b9279455ed673c6e6527fe960205bbc17bb2
Saif Ali <saif.tharani95@gmail.com>
2020-07-11 02:14:49 +0500

    Building tour page.

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -4,6 +4,6 @@ const viewsController = require('../controllers/viewsController');
 const viewRouter = express.Router();
 
 viewRouter.get('/', viewsController.getOverview);
-viewRouter.get('/tour', viewsController.getTour);
+viewRouter.get('/tour/:slug', viewsController.getTour);
 
 module.exports = viewRouter;
