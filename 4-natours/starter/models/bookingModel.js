03be421d6c576a24798836ec3969818cb3848730
Saif Ali <saif.tharani95@gmail.com>
2020-07-17 04:06:36 +0500

    Booking Model file created.

---
+++ 4-natours/starter/models/bookingModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+
+const bookingSchema = new mongoose.Schema({
+    tour: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'Booking must belong to a tour'],
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Booking must belong to particular user'],
+    },
+    createdAt: {
+        type: 'Date',
+        default: Date.now(),
+    },
+    price: {
+        type: Number,
+        required: [true, 'Booking must have a price'],
+    },
+    paid: {
+        type: Boolean,
+        default: true,
+    },
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+    toObject: {
+        virtuals: true,
+    },
+});
+
+bookingSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'user',
+    }).populate({
+        path: 'tour',
+        select: 'name',
+    });
+    next();
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
