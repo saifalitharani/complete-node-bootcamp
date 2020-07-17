3c20a58908ad64ceaebf2b32eed8dfa34d6eacd4
Saif Ali <saif.tharani95@gmail.com>
2020-07-17 22:12:20 +0500

    Implemented CRUD operations for booking.

--- 4-natours/starter/routes/bookingRoutes.js
+++ 4-natours/starter/routes/bookingRoutes.js
@@ -1,11 +1,25 @@
 const express = require('express');
+
 const authController = require('../controllers/authController');
 const bookingController = require('../controllers/bookingController');
+
 const router = express.Router();
 
-router.get(
-    '/checkout_session/:tourID',
-    authController.protect,
-    bookingController.getCheckoutSession
-);
+router.use(authController.protect);
+
+router.get('/checkout_session/:tourID', bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('guide', 'lead-guide'));
+
+router
+    .route('/')
+    .get(bookingController.getAllBookings)
+    .post(bookingController.createBooking);
+
+router
+    .route('/:id')
+    .get(bookingController.getBooking)
+    .patch(bookingController.updateBooking)
+    .delete(bookingController.deleteBooking);
+
 module.exports = router;
